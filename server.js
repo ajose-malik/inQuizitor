4d26658f2762407e62cd1c666ff83d47492f744e
ajose-malik <ajose.malik@gmail.com>
2021-01-28 15:25:32 -0500

    Fixed git error

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-require("dotenv").config();
-const PORT = process.env.PORT;
+require('dotenv').config();
+const { PORT } = process.env || 3001;
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
-    console.log("Listening on port " + PORT);
-})
+	console.log(`STARTED PORT ${PORT}`);
+});
 
-const quizController = require("./controllers/quiz_controller.js");
-app.use("/quiz", quizController);
+const quizController = require('./controllers/quiz_controller.js');
+app.use('/quiz', quizController);
 
 //connect Mongo
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+});
 // Error / success
 mongoose.connection.on('error', err =>
-    console.log(
-        err.message,
-        ' is Mongod not running?/Problem with Atlas Connection?'
-    )
-)
-mongoose.connection.on('connected', () =>
-    console.log('mongo connected: ', MONGODB_URI)
-)
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+	console.log(
+		err.message,
+		' is Mongod not running?/Problem with Atlas Connection?'
+	)
+);
+mongoose.connection.on('connected', () => console.log('STARTED MONGODB'));
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
