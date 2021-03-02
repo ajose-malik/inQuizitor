55f515f4eb51cf6b8256310b7db129370e7f33c7
ajose-malik <ajose.malik@gmail.com>
2021-03-02 11:04:03 -0500

    Updated style

--- public/js/App.js
+++ public/js/App.js
@@ -1,28 +1,28 @@
 class App extends React.Component {
 	state = {
 		points: 0
-	};
+	}
 
 	playAgain = () => {
-		this.setState({ points: 0 });
-	};
+		this.setState({ points: 0 })
+	}
 
 	gameOver = () => {
-		this.setState({ points: -1 });
-	};
+		this.setState({ points: -1 })
+	}
 
 	incrementPoints = () => {
-		const { points } = this.state;
-		this.setState({ points: points + 1 });
-	};
+		const { points } = this.state
+		this.setState({ points: points + 1 })
+	}
 
 	decrementPoints = () => {
-		const { points } = this.state;
-		this.setState({ points: points - 1 });
-	};
+		const { points } = this.state
+		this.setState({ points: points - 1 })
+	}
 
 	render() {
-		const { points } = this.state;
+		const { points } = this.state
 
 		if (points > -1 && points < 10) {
 			return (
@@ -33,18 +33,13 @@ class App extends React.Component {
 					gameOver={this.gameOver}
 					playAgain={this.playAgain}
 				/>
-			);
+			)
 		} else if (points === 10) {
-			return <Winner playAgain={this.playAgain} />;
+			return <Winner playAgain={this.playAgain} />
 		} else {
-			return <Loser playAgain={this.playAgain} />;
+			return <Loser playAgain={this.playAgain} />
 		}
 	} //End of render
 } //End of class
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"))
--- public/js/components/winner.js
+++ public/js/components/winner.js
@@ -1,18 +1,18 @@
 const Winner = props => {
 	return (
-		<div className='d-flex row justify-content-center m-0 text-center p-5'>
+		<div className="d-flex row justify-content-center margin-top text-center p-5">
 			<img
-				src='https://media1.tenor.com/images/c9c828a12a5cfb5133479d4eac74c9f6/tenor.gif?itemid=15171828'
-				alt=''
-				className='p-0 btn-width'
+				src="https://media1.tenor.com/images/c9c828a12a5cfb5133479d4eac74c9f6/tenor.gif?itemid=15171828"
+				alt=""
+				className="p-0 btn-width"
 			/>
-			<h1 className='winner text-danger mt-5'>CONGRATULATIONS!</h1>
+			<h1 className="winner ylw-text-color mt-5">CONGRATULATIONS!</h1>
 
 			<button
 				onClick={props.playAgain}
-				className='bg-btn-color ylw-text-color px-4 m-2 py-3 btn-width'>
+				className="bg-btn-color ylw-text-color px-4 m-2 py-3 btn-width">
 				<h1>Play Again?</h1>
 			</button>
 		</div>
-	);
-};
+	)
+}
